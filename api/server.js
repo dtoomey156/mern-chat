9175e70cdac75605c6378d4cda28a645cfce20ec
Daniel <toomey.d.p@gmail.com>
2024-01-04 19:02:19 -0600

    Connect MongoDB database
    Database connected via Mongoose. Environment variables for connection created

--- api/server.js
+++ api/server.js
@@ -1,12 +1,29 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URL);
+
+// this db variable is only used to log out the connection success or failure
+const db = mongoose.connection;
 
 app.get("/test", (req, res) => {
-    res.json("poopy")
-})
+    res.json("poopy");
+});
 
-app.post("/register", (req, res) => {})
+app.post("/register", (req, res) => {});
 
-app.listen(4040, () => {
-    console.log("server listening on 4040")
-})
+// MongoDB via Mongoose connection confirmation
+
+db.once("open", (_) => {
+    console.log(`Database connected on ${process.env.MONGO_URL}`);
+});
+
+db.on("error", (err) => {
+    console.error("Database connection error", err);
+});
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`server listening on port ${process.env.SERVER_PORT}`);
+});
