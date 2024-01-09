b9db2534f493b345d19ace7f70d4fb113ea88bb8
Daniel <toomey.d.p@gmail.com>
2024-01-09 13:58:37 -0600

    Implement login functionality
    JS - Routes for register created. Route for currently registered user auto login via JWT
    
    React - Register component adapted to also be the login for existing users

--- api/server.js
+++ api/server.js
@@ -1,18 +1,72 @@
 const express = require("express");
 const app = express();
+const UserModel = require("./models/User");
 require("dotenv").config();
-
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URL);
+const jwtSecret = process.env.JWT_SECRET;
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 // this db variable is only used to log out the connection success or failure
 const db = mongoose.connection;
+
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.CLIENT_URL,
+    })
+);
+
+app.use(express.json());
+
+app.use(cookieParser());
 
 app.get("/test", (req, res) => {
     res.json("poopy");
 });
 
-app.post("/register", (req, res) => {});
+app.get("/profile", (req, res) => {
+    const token = req.cookies?.token;
+    if (token) {
+        jwt.verify(token, jwtSecret, {}, (err, userData) => {
+            if (err) throw err;
+            res.json(userData);
+        });
+    } else {
+        res.status(401).json("no token");
+    }
+});
+
+app.post("/register", async (req, res) => {
+    const { username, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
+    const createdUser = await UserModel.create({
+        username: username,
+        password: hashedPassword,
+    });
+    jwt.sign(
+        { userId: createdUser._id, username },
+        jwtSecret,
+        {},
+        (err, token) => {
+            if (err) throw err;
+            res.cookie("token", token, { sameSite: "none", secure: true })
+                .status(201)
+                .json({
+                    id: createdUser._id,
+                });
+        }
+    );
+});
+
+app.post("/login", async (req, res) => {
+    const { password, username } = req.body;
+    const foundUser = await UserModel.findOne({ username });
+});
 
 // MongoDB via Mongoose connection confirmation
 
