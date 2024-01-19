b4bd1e0e057d0f641b7ded3021721a105914d313
Daniel <toomey.d.p@gmail.com>
2024-01-19 11:57:22 -0600

    Implement backend for Websocket messaging
    JWT - token authentication
    WSS - newotification of connected users. Routing of message object to connected users
    MongoDB - message schema created and inserted into DB

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const User = require("./models/User");
+const Message = require("./models/Message");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
@@ -42,8 +43,7 @@ app.get("/profile", (req, res) => {
             res.json(userData);
         });
     } else {
-        // res.status(401).json("no token");
-        return;
+        res.status(201);
     }
 });
 
@@ -106,7 +106,66 @@ const server = app.listen(process.env.SERVER_PORT, () => {
     console.log(`server listening on port ${process.env.SERVER_PORT}`);
 });
 
+// set up the websocket server and read unsername and id from the cookie
+
 const wss = new ws.WebSocketServer({ server });
-wss.on("connection", (connection) => {
-    console.log("websocket connected");
+wss.on("connection", (connection, req) => {
+    const cookies = req.headers.cookie;
+    if (cookies) {
+        const tokenCookieString = cookies
+            .split(";")
+            .find((str) => str.startsWith("token="));
+        if (tokenCookieString) {
+            const token = tokenCookieString.split("=")[1];
+            if (token) {
+                jwt.verify(token, jwtSecret, {}, (err, userData) => {
+                    if (err) throw err;
+                    const { userId, username } = userData;
+                    connection.userId = userId;
+                    connection.username = username;
+                });
+            }
+        }
+    }
+
+    connection.on("message", async (message) => {
+        const messageData = JSON.parse(message.toString());
+        console.log(
+            "message data sent through server from send Message",
+            messageData
+        );
+        const { recipient, text } = messageData;
+        if (recipient && text) {
+            const messageDoc = await Message.create({
+                sender: connection.userId,
+                recipient,
+                text,
+            });
+            [...wss.clients]
+                .filter((c) => c.userId === recipient)
+                .forEach((c) =>
+                    c.send(
+                        JSON.stringify({
+                            text,
+                            sender: connection.userId,
+                            recipient,
+                            id: messageDoc._id,
+                        })
+                    )
+                );
+        }
+    });
+
+    // notifies everyone about online people when someone connects
+
+    [...wss.clients].forEach((client) => {
+        client.send(
+            JSON.stringify({
+                online: [...wss.clients].map((client) => ({
+                    userId: client.userId,
+                    username: client.username,
+                })),
+            })
+        );
+    });
 });
